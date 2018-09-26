a78c36dd23cd35cf67cb0cc11c0803cd025ec4f7
alyssackieffer <alyssackieffer@Alyssas-MacBook-Pro.local>
2018-09-26 11:40:39 -0400

    Attempting functionality take 9

--- Unit_1/app.js
+++ Unit_1/app.js
@@ -1,40 +1,132 @@
 //Beginning of code
-$(()=>{
-//Beginning of code
+$(() => {
+  //Beginning of code
 
-var game = {
-  level: 1,
-  score: 0,
-  active: false,
-  handler: false,
-  shape: '.color',
-  genSeq: [],
-  userSeq: [],
+userSeq = [];
+compSeq = [];
+
+const levels = 20;
+
+var id, color, level = 0;
+
+//Start button listener - generate comp sequence, listen for user sequence and level up
+$(document).ready(function(){
+  $('.level').text('0');
+  $('.start').on('click', ()=>{
+    level = 0;
+    level++;
+    compSeq = [];
+    userSeq =  [];
+  compSequence();
+  })
+
+  $('.button').on('click', ()=>{
+    id = $(this).attr('id');
+    pad = $(this).attr('class').split(' ')[1];
+  userSequence();
+  })
+});
+
+//Listen for userSequence
+function userSequence(){
+  userSeq.push(id);
+  console.log(id + " " + color);
+  if(!checkUserSeq()){
+    error = true;
+    displayError();
+    userSeq = [];
+    computerSequence();
+    //Check sequence
+  } else if (userSeq.length === compSeq.length && userSeq.length < level){
+    level++;
+    userSeq = [];
+    error = false;
+    console.log('Start game')
+    computerSequence();
+  }
 }
 
-function flash(element, times, speed, .color){
-  var this = that;
-  if (times > 0){
-    element.stop().animate(opacity: '1'), {
-      duration: 50,
-      complete: function(){
-        element.stop().animate({opacity: '0.6'}, 200);
-      }
+//Check win state
+if (userSeq.length === level){
+  displayWinner();
+}
+
+//Comp generated sequence
+function computerSequence(){
+  console.log(level);
+  $('.level').text('level');
+  if(!error){
+    randomNum();
+  }
+  if (error){
+    randomNum();
+  }
+  var i = 0;
+  var myInt = setInt(function(){
+    id = compSeq[i];
+    color = $('#' + id).attr('class');
+    color = color.split(' ')[1];
+    console.log(id + ' ' + color);
+    i++;
+    if (i == compSeq.length){
+      clearInt(myInt);
+    }
+  }, 1000);
+}
+
+//Generate random number
+function randomNum(){
+  var random = Math.floor(Math.random() * 4);
+  compSeq.push(random);
+}
+
+//Class for flash
+function addClass(id, color){
+  $('#' + id).addClass(color + '-active');
+  setTimeout(function(){
+    $('#' + id).removeClass(color + '-active');
+  }, 500);
+}
+
+//User vs. computerSequence for win state
+function checkUserSeq(){
+  for (var i = 0; i < userSeq.length; i++){
+    if (userSeq[i] != compSeq[i]){
+      return false;
     }
   }
-  if (times > 0){
-    setTimeout(function() {
-      that.flash(element, times, speed, .color);
-    }, speed);
-    times -= 1;
-  }
-},
+  return true;
+}
 
-function genPattern(flash) {
-  for (i = 0; i < flash; i++){
-    this.genPattern.push(Math.floor(Math.random() * 4) +1);
-  }
-},
+//Error
+function displayError() {
+  console.log('Error!');
+  var counter = 0;
+  var myError = setInt(function(){
+    $('.level').text('Err');
+    counter++;
+    if (counter == 3){
+      $('.level').text(level);
+      clearInt(myError);
+        userSeq = [];
+        counter = 0;
+    }
+  }, 500);
+}
+
+//Win state display
+function displayWin(){
+  var count = 0;
+  var winInt = setInt(function(){
+    count++;
+    $('.level').text('Win');
+    if (count == 5){
+      clearInt(winInt);
+      $('.level').text('00');
+        count = 0;
+    }
+  }, 500);
+}
 
 //End of code
 });
