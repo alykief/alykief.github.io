b2f73616b5023a422d027a51cc9cf8e25fea9000
alyssackieffer <alyssackieffer@Alyssas-MacBook-Pro.local>
2018-09-25 23:10:40 -0400

    Working on functionality

--- Unit_1/app.js
+++ Unit_1/app.js
@@ -2,6 +2,40 @@
 $(()=>{
 //Beginning of code
 
+var game = {
+  level: 1,
+  score: 0,
+  active: false,
+  handler: false,
+  shape: '.color',
+  genSeq: [],
+  userSeq: [],
+}
+
+function flash(element, times, speed, .color){
+  var this = that;
+  if (times > 0){
+    element.stop().animate(opacity: '1'), {
+      duration: 50,
+      complete: function(){
+        element.stop().animate({opacity: '0.6'}, 200);
+      }
+    }
+  }
+  if (times > 0){
+    setTimeout(function() {
+      that.flash(element, times, speed, .color);
+    }, speed);
+    times -= 1;
+  }
+},
+
+function genPattern(flash) {
+  for (i = 0; i < flash; i++){
+    this.genPattern.push(Math.floor(Math.random() * 4) +1);
+  }
+},
+
 //End of code
 });
 //End of code
